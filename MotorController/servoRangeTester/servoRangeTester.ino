cd6ad4a9bc470c7d5c3381f631a84aa132ac2da1
Felix Epp <study@felixepp.de>
2014-11-06 12:30:58 +0100

    servoRangeTester resets servos

--- MotorController/servoRangeTester/servoRangeTester.ino
+++ MotorController/servoRangeTester/servoRangeTester.ino
@@ -1,18 +1,18 @@
-/*************************************************** 
+/***************************************************
   This is an example for our Adafruit 16-channel PWM & Servo driver
   Servo test - this will drive 16 servos, one after the other
 
   Pick one up today in the adafruit shop!
   ------> http://www.adafruit.com/products/815
 
-  These displays use I2C to communicate, 2 pins are required to  
+  These displays use I2C to communicate, 2 pins are required to
   interface. For Arduino UNOs, thats SCL -> Analog 5, SDA -> Analog 4
 
-  Adafruit invests time and resources providing this open source code, 
-  please support Adafruit and open-source hardware by purchasing 
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
   products from Adafruit!
 
-  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  Written by Limor Fried/Ladyada for Adafruit Industries.
   BSD license, all text above must be included in any redistribution
  ****************************************************/
 
@@ -24,14 +24,18 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 // you can also call it with a different address you want
 //Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x41);
 
-// Depending on your servo make, the pulse width min and max may vary, you 
+// Depending on your servo make, the pulse width min and max may vary, you
 // want these to be as small/large as possible without hitting the hard stop
 // for max range. You'll have to tweak them as necessary to match the servos you
 // have!
 #define SERVOMIN  150 // this is the 'minimum' pulse length count (out of 4096)
 #define SERVOMAX  670 // this is the 'maximum' pulse length count (out of 4096)
 
-int servonum = 0;
+
+int servoMin[16] = {540, 510, 170, 150,
+                    150, 650, 150, 670};
+
+int servonum = 6;
 
 void setup() {
   Serial.begin(9600);
@@ -40,8 +44,10 @@ void setup() {
   Serial.println(" by Serial Send");
 
   pwm.begin();
-  
+
   pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
+
+  resetServos();
 }
 
 void loop() {
@@ -50,10 +56,19 @@ void loop() {
     // read the incoming int:
     tangle = Serial.parseInt();
     if(tangle <= SERVOMAX && tangle >= SERVOMIN) {
-      pwm.setPWM(servonum, 0, tangle); 
+      pwm.setPWM(servonum, 0, tangle);
     }
   }
-  
+
   //safety delay
   delay(5);
+}
+
+// Drive Servos to idle position
+void resetServos()
+{
+  for (int servonum = 0; servonum < 8; servonum++) {
+    // Drive each servo one at a time
+    pwm.setPWM(servonum, 0, servoMin[servonum]);
+  }
 }
